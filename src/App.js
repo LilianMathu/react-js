9e9e5a45a648d9803f9b60d489c197b9cffa5e3f
Lilian Mathu <lilianmathu2018@gmail.com>
2022-08-08 17:51:01 +0300

    react props - class and functional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import "./App.css";
+import Welcome from "./Lessons/classProps";
+import People from "./Lessons/functionalProps";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hello World
-      </h1>
+      <People name="Lilian">
+        <button>Submit</button>
+      </People>
+      <Welcome name="Mathu">
+        <button>Click me!</button>
+      </Welcome>
     </div>
   );
 }
