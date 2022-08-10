99d8a12f39d1f53bf8d5c223c25eb536c0b0216d
Lilian Mathu <lilianmathu2018@gmail.com>
2022-08-10 07:55:00 +0300

    State components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import Welcome from "./Lessons/classProps";
-import People from "./Lessons/functionalProps";
+import Welcome from "./Lessons/StateLesson";
 
 function App() {
   return (
     <div className="App">
-      <People name="Lilian">
-        <button>Submit</button>
-      </People>
-      <Welcome name="Mathu">
-        <button>Click me!</button>
-      </Welcome>
+      <Welcome />
     </div>
   );
 }
