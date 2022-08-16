8db65c2f1566cc3fb9db5bd03b5d394c323d5183
Lilian Mathu <lilianmathu2018@gmail.com>
2022-08-16 12:13:03 +0300

    list rendering using map()

--- src/Lessons/ListRendering.js
+++ src/Lessons/ListRendering.js
@@ -1,9 +1,40 @@
 import React from "react";
 
 const ListRendering = () => {
-  const names = ["Lilian", "Adele", "Mathu"];
-  const nameList = names.map((name) => <p>{name}</p>);
-  return <div>{nameList}</div>;
+  const persons = [
+    {
+      name: "Lilian",
+      age: 20,
+      married: false,
+      skills: {
+        frontend: "react",
+        database: "mongo",
+        serverside: "express/node",
+      },
+    },
+    {
+      name: "Mathu",
+      age: 50,
+      married: true,
+      skills: {
+        frontend: "vue",
+        database: "mysql",
+        serverside: "express/node",
+      },
+    },
+  ];
+
+  const personList = persons.map((person) => {
+    return (
+      <h4>
+        My name is {person.name}. I am {person.age} years old. My skills include{" "}
+        {person.skills.frontend}, {person.skills.database},{" "}
+        {person.skills.serverside}
+      </h4>
+    );
+  });
+
+  return <div>{personList}</div>;
 };
 
 export default ListRendering;
