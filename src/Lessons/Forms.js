3212ae547da69e08ea60615857c2a01ad418f128
Lilian Mathu <lilianmathu2018@gmail.com>
2022-09-02 00:57:37 +0300

    Form handling - input, textarea, select, submit

--- src/Lessons/Forms.js
+++ src/Lessons/Forms.js
@@ -5,27 +5,62 @@ class Forms extends Component {
     super(props);
 
     this.state = {
-      name: "",
+      username: "",
+      comment: "",
+      topic: "react",
     };
   }
 
-  handleNameChange = (event) => {
+  handleUsernameChange = (event) => {
     this.setState({
-      name: event.target.value,
+      username: event.target.value,
     });
+  };
+
+  handleCommentChange = (event) => {
+    this.setState({
+      comment: event.target.value,
+    });
+  };
+
+  handleTopicChange = (event) => {
+    this.setState({
+      topic: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    alert(`${this.state.username}, ${this.state.comment}, ${this.state.topic}`);
+    event.preventDefault();
   };
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>Username: </label>
           <input
             type="text"
-            value={this.state.name}
-            onChange={this.handleNameChange}
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
           />
         </div>
+        <div>
+          <label>Comment: </label>
+          <textarea
+            value={this.state.comment}
+            onChange={this.handleCommentChange}
+          ></textarea>
+        </div>
+        <div>
+          <label>Topic: </label>
+          <select value={this.state.topic} onChange={this.handleTopicChange}>
+            <option value="react">React</option>
+            <option value="Vue">Vue</option>
+            <option value="angular">Angular</option>
+          </select>
+        </div>
+        <button type="submit">Submit</button>
       </form>
     );
   }
